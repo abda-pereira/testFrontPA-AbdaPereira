61dd0d818a107c641f0b93b6d75846dbae3f5072
Abda Pereira <abdapereira@hotmail.com>
2018-05-09 12:53:22 -0300

    first commit

---
+++ js/testfront.js
@@ -0,0 +1,18 @@
+(function($) {
+  $('.hide-menu-responsive').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+
+  navbarCollapse();
+
+  $(window).scroll(navbarCollapse);
+
+})(jQuery);
